7ce82537e13940cd939cd1c3091356c6778eed19
iamWGO <gottlieb.waleerat@gmail.com>
2024-03-25 13:07:37 +0100

    fix check-installed

--- vsp/check-installed.sh
+++ vsp/check-installed.sh
@@ -4,7 +4,8 @@ source ../source/message.sh
 is_installed() {
     package=$1
     #dpkg -s $pakcage &> /dev/null
-    if dpkg -l | grep -q $package; then
+    #if dpkg -l | grep -q $package; then
+    if which $package; then
         return 1
     else
         return 0
