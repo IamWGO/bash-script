01ef0c5acb8b10492388051abc829f69364b8c9e
iamWGO <gottlieb.waleerat@gmail.com>
2024-03-25 13:04:15 +0100

    fix htop.sh

--- vsp/task/htop.sh
+++ vsp/task/htop.sh
@@ -2,7 +2,7 @@
 source ../source/message.sh
 source ./check-installed.sh
 
-installed=$(is_installed $PACKAGE)
+installed=$(is_installed htop)
 
 if [[ $installed==0 ]]; then
     message "do process" blue true
